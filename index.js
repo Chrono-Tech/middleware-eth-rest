235b23e4e1ed1c2ff921080b97ea9df0397b5ef3
egor <zyev.egor@rambler.ru>
2018-08-11 00:07:50 +0300

    https://chronobank.atlassian.net/browse/MD-577 - expand eth rest

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ const config = require('./config'),
 
 mongoose.Promise = Promise;
 mongoose.accounts = mongoose.createConnection(config.mongo.accounts.uri);
-
-if (config.mongo.data.useData)
-  mongoose.data = mongoose.createConnection(config.mongo.data.uri);
+mongoose.data = mongoose.createConnection(config.mongo.data.uri);
 
 const init = async () => {
 
